fc99ead258ef1b87c89a378548c15df0341ae911
Gustavs Bergs <gustavs.bergs@accenture.com>
2019-01-15 13:28:38 +0200

    Initial commit

---
+++ src/main/java/com/bergsgustavs/todotrainingproject/TodoTrainingProjectApplication.java
@@ -0,0 +1,14 @@
+package com.bergsgustavs.todotrainingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoTrainingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoTrainingProjectApplication.class, args);
+	}
+
+}
+
