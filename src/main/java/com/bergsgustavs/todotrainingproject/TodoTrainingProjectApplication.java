903f701419acd47d50bdc8af10aa405ebb649eb1
gustavs.bergs <bergsgustavs@gmail.com>
2019-02-11 15:57:58 +0200

    Ready to deploy

--- src/main/java/com/bergsgustavs/todotrainingproject/TodoTrainingProjectApplication.java
+++ src/main/java/com/bergsgustavs/todotrainingproject/TodoTrainingProjectApplication.java
@@ -4,10 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class TodoTrainingProjectApplication {
+public class TodoTrainingProjectApplication extends SpringBootServletInitializer {
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TodoTrainingProjectApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TodoTrainingProjectApplication.class, args);
